827f3b6e68d901ab8f7e6b8dc9d1ffb73ddcf1da
action <action@github.com>
2024-06-28 04:42:48 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,1 +1,22 @@
-var _0x3bb7 = ['Gm17w7LDqwfCo8K6YsOYwonDqTc7bhk=', 'LUnDg8KuTMOCJw==', 'w4LCocOqwrJqHi3Dtl0BwqXDuVg=', 'fMKqREg=', 'ZMKkYBZyahA=', 'w6PDj3Ulwp95wow=', 'PMKuwrjDtj3CkcKbZ8KqRGLDsXtW', 'wrPDjmo+wpBWwobDlEsqwqvCuCYtacKQw5nDjg==', 'NcKQIcKPwoNpcTXCsULCtcOm5byl5Lmg5puj6KWR6ZWYUg==', 'QWIxQlXClmM=', 'wp3Cr8K4wqsPwrwJW8KwL8OFw6glRsKbaQLCgA==', 'Rxp1RcK3HDzDsG5iKMOGw7Zaw6DDgA==', 'XMKUN3w/w5ZlV1LCgsObZ8OGw4U=', 'JcKsw7LCslt0wpE=', 'wqLChAoU', 'wqwIaMOrZMKuw5A=', 'w7nDqwzDocOBw7dm', 'M8K/Z8Kjw4nCsWoLDGzCkMKsDMKJ5beW5rOb5LuKwr0=', 'wrLCjh4BwoHDvBw=', 'ZQvDssKFMsKCVg=='];
+{
+  'Gm17w7LDqwfCo8K6YsOYwonDqTc7bhk=';
+  'LUnDg8KuTMOCJw==';
+  'w4LCocOqwrJqHi3Dtl0BwqXDuVg=';
+  'fMKqREg=';
+  'ZMKkYBZyahA=';
+  'w6PDj3Ulwp95wow=';
+  'PMKuwrjDtj3CkcKbZ8KqRGLDsXtW';
+  'wrPDjmo+wpBWwobDlEsqwqvCuCYtacKQw5nDjg==';
+  'NcKQIcKPwoNpcTXCsULCtcOm5byl5Lmg5puj6KWR6ZWYUg==';
+  'QWIxQlXClmM=';
+  'wp3Cr8K4wqsPwrwJW8KwL8OFw6glRsKbaQLCgA==';
+  'Rxp1RcK3HDzDsG5iKMOGw7Zaw6DDgA==';
+  'XMKUN3w/w5ZlV1LCgsObZ8OGw4U=';
+  'JcKsw7LCslt0wpE=';
+  'wqLChAoU';
+  'wqwIaMOrZMKuw5A=';
+  'w7nDqwzDocOBw7dm';
+  'M8K/Z8Kjw4nCsWoLDGzCkMKsDMKJ5beW5rOb5LuKwr0=';
+  'wrLCjh4BwoHDvBw=';
+  'ZQvDssKFMsKCVg==';
+}
