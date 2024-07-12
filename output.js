eb39aa8921d1da4c1c9a176f99ec74b435cfd549
action <action@github.com>
2024-07-12 14:21:53 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,75 +1,220 @@
-//7/12/2024, 9:51:40 PM
+//7/12/2024, 10:21:53 PM
 //Author:https://github.com/lck7
-let chxm1023 = JSON.parse($response.body);
-chxm1023 = {
-  "status": "0",
-  "receipt-data": {
-    "status": 0,
-    "environment": "Production",
-    "receipt": {
-      "receipt_type": "Production",
-      "app_item_id": 1065511007,
-      "receipt_creation_date": "2019-10-30 16:52:23 Etc\\\\/GMT",
-      "bundle_id": "com.globaldelight.iBoom",
-      "original_purchase_date": "2019-04-17 04:07:39 Etc\\\\/GMT",
-      "in_app": [{
-        "quantity": "1",
-        "purchase_date_ms": "1555474256000",
-        "expires_date": "2099-09-09 09:09:09 Etc\\\\/GMT",
-        "expires_date_pst": "2099-09-09 09:09:09 America\\\\/Los_Angeles",
-        "is_in_intro_offer_period": "false",
-        "transaction_id": "470000445785125",
-        "is_trial_period": "false",
-        "original_transaction_id": "470000445785125",
-        "purchase_date": "2019-04-17 04:10:56 Etc\\\\/GMT",
-        "product_id": "com.globaldelight.iBoom.LifetimeDiscountPack",
-        "original_purchase_date_pst": "2019-04-16 21:10:59 America\\\\/Los_Angeles",
-        "original_purchase_date_ms": "1555474259000",
-        "web_order_line_item_id": "470000137081235",
-        "expires_date_ms": "4092599350000",
-        "purchase_date_pst": "2019-04-16 21:10:56 America\\\\/Los_Angeles",
-        "original_purchase_date": "2019-04-17 04:10:59 Etc\\\\/GMT"
-      }],
-      "adam_id": 1065511007,
-      "receipt_creation_date_pst": "2019-10-30 09:52:23 America\\\\/Los_Angeles",
-      "request_date": "2019-10-30 16:52:29 Etc\\\\/GMT",
-      "request_date_pst": "2019-10-30 09:52:29 America\\\\/Los_Angeles",
-      "version_external_identifier": 832251566,
-      "request_date_ms": "1572454349573",
-      "original_purchase_date_pst": "2019-04-16 21:07:39 America\\\\/Los_Angeles",
-      "application_version": "1.4.70002",
-      "original_purchase_date_ms": "1555474059000",
-      "receipt_creation_date_ms": "1572454343000",
-      "original_application_version": "1.4.10008",
-      "download_id": 87042883772350
-    },
-    "latest_receipt_info": [{
-      "quantity": "1",
-      "purchase_date_ms": "1555474256000",
-      "expires_date": "2099-09-09 09:09:09 Etc\\\\/GMT",
-      "expires_date_pst": "2099-09-09 09:09:09 America\\\\/Los_Angeles",
-      "is_in_intro_offer_period": "false",
-      "transaction_id": "470000445785125",
-      "is_trial_period": "false",
-      "original_transaction_id": "470000445785125",
-      "purchase_date": "2019-04-17 04:10:56 Etc\\\\/GMT",
-      "product_id": "com.globaldelight.iBoom.LifetimeDiscountPack",
-      "original_purchase_date_pst": "2019-04-16 21:10:59 America\\\\/Los_Angeles",
-      "subscription_group_identifier": "20461753",
-      "original_purchase_date_ms": "1555474259000",
-      "web_order_line_item_id": "470000137081235",
-      "expires_date_ms": "4092599350000",
-      "purchase_date_pst": "2019-04-16 21:10:56 America\\\\/Los_Angeles",
-      "original_purchase_date": "2019-04-17 04:10:59 Etc\\\\/GMT"
-    }],
-    "pending_renewal_info": [{
-      "product_id": "com.globaldelight.iBoom.LifetimeDiscountPack",
-      "original_transaction_id": "470000445785125",
-      "auto_renew_product_id": "com.globaldelight.iBoom.LifetimeDiscountPack",
-      "auto_renew_status": "0"
-    }]
-  }
-};
-$done({
-  body: JSON.stringify(chxm1023)
+var _0x205bdf = function () {
+  var _0x984da5 = true;
+  return function (_0x351317, _0x33ae9a) {
+    var _0x3d23c6 = _0x984da5 ? function () {
+      if (_0x33ae9a) {
+        var _0x374666 = _0x33ae9a.apply(_0x351317, arguments);
+        _0x33ae9a = null;
+        return _0x374666;
+      }
+    } : function () {};
+    _0x984da5 = false;
+    return _0x3d23c6;
+  };
+}();
+var _0x5e0465 = _0x205bdf(this, function () {
+  var _0x23164b = function () {
+    var _0x40a988 = _0x23164b.constructor("return /\" + this + \"/")().compile("^([^ ]+( +[^ ]+)+)+[^ ]}");
+    return !_0x40a988.test(_0x5e0465);
+  };
+  return _0x23164b();
 });
+_0x5e0465();
+var _0x2a4515 = function () {
+  var _0x4c1c50 = true;
+  return function (_0x27261c, _0x44e183) {
+    var _0xa0bf7c = _0x4c1c50 ? function () {
+      if (_0x44e183) {
+        var _0xe8b048 = _0x44e183.apply(_0x27261c, arguments);
+        _0x44e183 = null;
+        return _0xe8b048;
+      }
+    } : function () {};
+    _0x4c1c50 = false;
+    return _0xa0bf7c;
+  };
+}();
+var _0x1c3b70 = _0x2a4515(this, function () {
+  var _0x6d8a0 = function () {};
+  var _0x47c549;
+  try {
+    var _0x5046f0 = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x47c549 = _0x5046f0();
+  } catch (_0x235cb9) {
+    _0x47c549 = window;
+  }
+  if (!_0x47c549.console) {
+    _0x47c549.console = function (_0x7f3980) {
+      var _0x4a14e4 = {};
+      _0x4a14e4.log = _0x7f3980;
+      _0x4a14e4.warn = _0x7f3980;
+      _0x4a14e4.debug = _0x7f3980;
+      _0x4a14e4.info = _0x7f3980;
+      _0x4a14e4.error = _0x7f3980;
+      _0x4a14e4.exception = _0x7f3980;
+      _0x4a14e4.table = _0x7f3980;
+      _0x4a14e4.trace = _0x7f3980;
+      return _0x4a14e4;
+    }(_0x6d8a0);
+  } else {
+    _0x47c549.console.log = _0x6d8a0;
+    _0x47c549.console.warn = _0x6d8a0;
+    _0x47c549.console.debug = _0x6d8a0;
+    _0x47c549.console.info = _0x6d8a0;
+    _0x47c549.console.error = _0x6d8a0;
+    _0x47c549.console.exception = _0x6d8a0;
+    _0x47c549.console.table = _0x6d8a0;
+    _0x47c549.console.trace = _0x6d8a0;
+  }
+});
+_0x1c3b70();
+var _0x42fcfe = $request.url;
+var _0x407d52 = $response.body;
+var _0xc5cbc6 = $request.method;
+const _0x4e26ef = "Serv";
+const _0x28be74 = "/a.p";
+const _0x2caaf2 = "/music.pay";
+const _0x1e0f8d = "/vip/v2/user/vip";
+const _0x5ab574 = "/vip/enc/user/vip";
+const _0x43d7dd = "/vip/v2/theme";
+const _0x21ada0 = "Vo4m6X2hTph/vfpPmau8PTT0sFN6JCgzxSLVH/u3sbEt7VniYsVHbRFgOgN+Uvs39rAI7R3C5HVpaSj8tr8U8dLYwYdDCjMILuUorh3z0BiQToiWxudHkcASIPHNrmZHZYC/yv3DP4b89hbzfqU5UUDUqaZpEBZr76sDF2wNPmYjUEFSVCMGyTl1F6j1DBmKJ1Tik0YuG/2UBa/Ilz12a1KneXsNs5x5EE41bXDke7EygIB3I+6SoITZXOLFAFQFZujdI0GzClNglDKtclpUxpjN3uVeJxHLU40FTwNWo3ZDNv8KSdZpYZ5BDEOCyZkifmHlf1wnocX2zTr2xRAM6JhAD2WaSSNQQVJUI5lv72QNZSN43Pj/qdzatHQP4Pp/H1YxyP36rv3qBcnnJy/55YouIczRc3eJjXExRgo54qdyTYRMYoS9GzNn/edR3hSNnMn9PnElBCfZhkL0R5kZ9JBFCM3vNOy7Cnp6RVyAG0GFHv/g2q1yqkJxibyDro5nlnnvHjhZrsOvSvTXI1BBUlQjGoRqqCTDUvHLoiNwWMoKKfxtswWQiXjoQ6mL5dazxjUsbsHzC1N8YNMVtzf8gBryr3nMWS44wyUpi1/0WhGTRW1wsCllO1DB24+ibTFH/yftWN+/apM9vbQAkc/J+aFy/01plK7rsGNwWYYKG0sr6CS8dGQzy0On6aFo07hiU+wjUEFSVCOf/wKzzX5Cn/OLMKeVa1BPDxV5tm39vCrsxIG6T29VHWx8ck93S/nXCm2dHfojuLySZKJ50B1FaN5uFIY+LA1RbO/0sL+CoSJhoNOLibzt75c5dleW+lbwxLAAdBh5AFq4Z1Uj8bPjm5mHcGWQuBAyZIO+ie8wP4yvWwQFf1ENJiNQQVJUIzwCo22cpAtoAzYZWm3XFPfSlov4G15JGaaHL2X5FG5BTeUwwbBiQfwUpcb6oT8dbIKh2SsUZCeJZW43lLI0UIo9u3y1+P4GMtOKEZ7Sx0aQ3ewknthU2tpL0gnykFtiEtKBxcfHjJEen158zVXrbxxC0W35SmaYOOwgAmEMfxwHI1BBUlQjhVUHnBabnJcnmXCICcyUBglrZkXcNLwg91p4889vKFTLlzROHTt20UzjfKWsNK3U8pYgKYXPbQtSzIuRheEEQDFhLvEhIGKaB6yDoacDLJZ0jgFRIKKFBkbK0VE4nIABi1qgQOXvq1sG4QeupjfEWYqMX8EyyqPHrsDiCltAF1wjUEFSVCNybeUusnxJF2zswj8xQtfPiwfDj3TwKWxKXCmkheqHy7/0Qpyc84xWvq+YXktsU97wUZLHrgJmARudJmQNEwAweIdHMafcwreBy731z6kGLojy5TLgTN7XSm5Ar+hgOW+1ZwkWLyrVvaCdO/8/zdYl1w/PQUCs6dw0ThIeahwjpQ==";
+if (_0x42fcfe.indexOf(_0x4e26ef) != -1 && _0xc5cbc6 == "GET") {
+  var _0x22c76e = {
+    body: "{}"
+  };
+  $done(_0x22c76e);
+}
+if (_0x42fcfe.indexOf(_0x28be74) != -1 && _0xc5cbc6 == "POST") {
+  var _0x407d52 = $response.body.replace(/"playright":\d+/g, "\"playright\":1").replace(/"downright":\d+/g, "\"downright\":1").replace(/"policytype":\d+/g, "\"policytype\":3").replace(/"policy":\d+/g, "\"policy\":5");
+  var _0xa90fcc = {
+    body: _0x407d52
+  };
+  $done(_0xa90fcc);
+} else {
+  if (_0x42fcfe.indexOf(_0x28be74) != -1 && _0x42fcfe.indexOf("getvip") != -1) {
+    var _0xf48cd5 = JSON.parse(_0x407d52);
+    var _0x5c24c6 = {
+      end: 32495443200,
+      bought_vip: 1,
+      type: 1,
+      period: 31,
+      bought_vip_end: 32495443200
+    };
+    _0xf48cd5.packs = _0x5c24c6;
+    _0x407d52 = JSON.stringify(_0xf48cd5);
+    var _0x2326b4 = {
+      body: _0x407d52
+    };
+    $done(_0x2326b4);
+  } else {
+    if (_0x42fcfe.indexOf(_0x28be74) != -1 && _0x42fcfe.indexOf("advertright") != -1) {
+      $done({});
+    }
+  }
+}
+if (_0x42fcfe.indexOf(_0x5ab574) != -1 && _0xc5cbc6 == "GET") {
+  var _0x1b6aa2 = {
+    body: _0x21ada0
+  };
+  $done(_0x1b6aa2);
+}
+if (_0x42fcfe.indexOf(_0x1e0f8d) != -1 && _0x42fcfe.indexOf("op=ui") != -1) {
+  var _0xf48cd5 = JSON.parse(_0x407d52);
+  var _0x259d14 = {
+    vipIcon: "https://image.kuwo.cn/fe/f2d09ac0-b959-404f-86fa-dc65c715c0e96.png",
+    iconJumpUrl: "http://vip1.kuwo.cn/vip/vue/anPay/pay/index.html?pageType=avip&MBOX_WEBCLOSE=1&FULLHASARROW=1",
+    growthValue: "21600",
+    vipTag: "VIP6",
+    vipOverSeasExpire: "0",
+    time: "1659582730304",
+    goSvipPage: "1",
+    isNewUser: "1",
+    vipmIcon: "https://image.kuwo.cn/fe/34ad47f8-da7f-43e4-abdc-e6c995666368yyb.png",
+    svipIcon: "https://image.kuwo.cn/fe/f2d09ac0-b959-404f-86fa-dc65c715c0e96.png",
+    vipmExpire: "32495443200000",
+    biedSong: "0",
+    luxuryIcon: "https://image.kuwo.cn/fe/2fae68ff-de2d-4473-bf28-8efc29e44968vip.png",
+    userType: "3",
+    isYearUser: "2",
+    vip3Expire: "0",
+    experienceExpire: "0",
+    luxAutoPayUser: "2",
+    biedAlbum: "1",
+    vipLuxuryExpire: "32495443200000",
+    vipmAutoPayUser: "2",
+    svipAutoPayUser: "2",
+    vipExpire: "32495443200000",
+    svipExpire: "32495443200000"
+  };
+  _0xf48cd5.data = _0x259d14;
+  _0x407d52 = JSON.stringify(_0xf48cd5);
+  var _0x1ce790 = {
+    body: _0x407d52
+  };
+  $done(_0x1ce790);
+} else {
+  if (_0x42fcfe.indexOf(_0x1e0f8d) != -1 && _0x42fcfe.indexOf("jsonpcallback") != -1) {
+    $done({});
+  }
+}
+if (_0x42fcfe.indexOf(_0x2caaf2) != -1 && _0xc5cbc6 == "POST") {
+  if (_0x407d52.indexOf("audio") != -1) {
+    var _0xf48cd5 = JSON.parse(_0x407d52);
+    var _0x507dcb = _0xf48cd5.songs[0].id;
+    var _0x20bb38 = _0xf48cd5.songs[0].audio[0].pid;
+    var _0x5392b0 = _0xf48cd5.songs[0].audio[0].price;
+    var _0x274ae8 = _0xf48cd5.songs[0].audio[0].policy;
+    var _0x108023 = _0x274ae8 + "_1";
+    var _0x20d61f = _0xf48cd5.songs[0].audio.length;
+    for (var _0x2644a8 = 0; _0x2644a8 < _0x20d61f; _0x2644a8++) {
+      _0xf48cd5.songs[0].audio[_0x2644a8].st = 0;
+    }
+    var _0x2dd3e8 = _0xf48cd5.songs[0];
+    var _0x27cbb9 = {
+      pid: _0x20bb38,
+      type: _0x274ae8,
+      name: _0x108023,
+      categray: _0x108023,
+      id: _0x507dcb,
+      order: 375787919,
+      final: [],
+      buy: 1657425321,
+      begin: 1657425321,
+      end: 4180305321,
+      CurEnd: 0,
+      playCnt: 0,
+      playUpper: 300,
+      downCnt: 0,
+      downUpper: 300,
+      playVideoCnt: 0,
+      playVideoUpper: 3000,
+      downVideoCnt: 0,
+      downVideoUpper: 3000,
+      price: _0x5392b0,
+      period: 1000,
+      feetype: 0,
+      info: _0x2dd3e8
+    };
+    _0xf48cd5.user = [_0x27cbb9];
+    var _0x575fea = {
+      body: JSON.stringify(_0xf48cd5)
+    };
+    $done(_0x575fea);
+  } else {
+    $done({});
+  }
+} else {
+  if (_0x42fcfe.indexOf(_0x2caaf2) != -1 && _0xc5cbc6 == "GET") {
+    $done({});
+  }
+}
+if (_0x42fcfe.indexOf(_0x43d7dd) != -1 && _0x42fcfe.indexOf("op=gd") != -1) {
+  var _0xf48cd5 = JSON.parse(_0x407d52);
+  _0xf48cd5.data.needBieds = null;
+  var _0x5525a6 = {
+    body: JSON.stringify(_0xf48cd5)
+  };
+  $done(_0x5525a6);
+}
